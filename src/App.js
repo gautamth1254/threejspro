c3060f94dad75a7b50a741d562218ea06829e63f
Gautam <gautam.th2002@gmail.com>
2022-06-25 17:56:51 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,76 @@
-import logo from './logo.svg';
 import './App.css';
+import styled from 'styled-components';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import Box from "./components/Box";
+
+import { Canvas } from "@react-three/fiber";
+import { OrbitControls } from "@react-three/drei";  // for animate a box
+
+
+
+export default function App(){
+  return(
+    <Wrapper>
+      <Canvas className="canvas">
+        <OrbitControls enableZoom={false}/>
+        <ambientLight intensity={0.5}/>
+        <directionalLight position={[-2,5,2]} intensity={1} />
+        <Box />
+      </Canvas>
+    </Wrapper>
+  )
 }
 
-export default App;
+const Wrapper = styled.div`
+    position: relative;
+    background: #1f1144;
+
+    canvas{
+      height:500px;
+      background:red;
+    }
+`;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// import logo from './logo.svg';
+// import './App.css';
+
+// function App() {
+//   return (
+//     <div className="App">
+//       <header className="App-header">
+//         <img src={logo} className="App-logo" alt="logo" />
+//         <p>
+//           Edit <code>src/App.js</code> and save to reload.
+//         </p>
+//         <a
+//           className="App-link"
+//           href="https://reactjs.org"
+//           target="_blank"
+//           rel="noopener noreferrer"
+//         >
+//           Learn React
+//         </a>
+//       </header>
+//     </div>
+//   );
+// }
+
+// export default App;
